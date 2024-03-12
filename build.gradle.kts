6f262cef9583e8265de19fdd607c77a813eaf8ed
Babur <bduisenov@users.noreply.github.com>
2024-03-12 10:20:24 +0100

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+	java
+	id("com.palantir.consistent-versions") version "2.21.0"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
